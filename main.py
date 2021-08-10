eec87bffc605e5e50252e4b2aa9f26107ae9cc83
Brian <brianzou03@gmail.com>
2021-08-10 15:15:07 -0700

    2nd commit. Web scraper now has a root element for all men's shirts and prints the prices.

--- main.py
+++ main.py
@@ -13,7 +13,19 @@ DRIVER_PATH = 'chromedriver.exe'
 
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 driver.get("https://www.pacsun.com/mens/shirts/")
-print(driver.page_source)
+# print(driver.page_source)
+
+root_element = driver.find_element_by_id("search-result-items")
+
+
+if root_element:
+    products = root_element.find_elements_by_tag_name("li")
+    for product in products:
+        price = product.find_element_by_class_name("price-standard")
+        # promo_price = product.find_element_by_class_name("price-promo")
+        # print(product.text)
+        print(price.text)
+        # print(promo_price.text)
 
 
 driver.quit()
