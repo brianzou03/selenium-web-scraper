7c1c935936563724ffa13b1b413f685dce2cb4ab
Brian <brianzou03@gmail.com>
2021-08-13 22:56:37 -0700

    4th Commit - Class PriceSearch appropriately renamed Scraper, get_name and get_link functions added.

--- main.py
+++ main.py
@@ -18,23 +18,34 @@ driver.get("https://www.pacsun.com/mens/shirts/")
 root_element = driver.find_element_by_id("search-result-items")
 
 
-class PriceSearch:
+class Scraper():
 
     def __init__(self):
         pass
 
-    def find_price(self):
+    def get_price(self, product):
+        return product.find_element_by_class_name("price-standard")
 
+    def get_name(self, name_link):
+        return name_link.get_attribute("title")
+
+    def get_link(self, name_link):
+        return name_link.get_attribute("href")
+
+    def find_element(self):
         if root_element:
             products = root_element.find_elements_by_tag_name("li")
             for product in products:
-                price = product.find_element_by_class_name("price-standard")
-                # print(product.text)
+                price = self.get_price(product)
+                name_link = product.find_element_by_class_name("name-link")
+                name = self.get_name(name_link)
+                link = self.get_link(name_link)
                 print(price.text)
+                print(name)
+                print(link)
 
 
-my_obj = PriceSearch()
-my_obj.find_price()
+my_obj = Scraper()
+my_obj.find_element()
 
 driver.quit()
-
