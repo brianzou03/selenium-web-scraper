a65771649721ff93683ba54ee8e5acaebe48f37a
Brian <brianzou03@gmail.com>
2021-08-14 23:41:42 -0700

    6th Commit - database support added to yaml, main.py refactored

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import yaml
+import psycopg2
 
 
 class Scraper():
@@ -14,6 +15,8 @@ class Scraper():
 
         for section in self.cfg:
             print(section)
+
+        self.connect_db()
 
         options = Options()
         options.headless = True
@@ -27,6 +30,16 @@ class Scraper():
 
         self.root_element = self.driver.find_element_by_id("search-result-items")
 
+    def connect_db(self):
+        self.connection = psycopg2.connect(
+            host=self.cfg["db"]["host"],
+            port=self.cfg["db"]["port"],
+            user=self.cfg["db"]["user"],
+            password=self.cfg["db"]["password"],
+            database=self.cfg["db"]["database"])
+
+        self.cursor = self.connection.cursor()
+
 
     def get_price(self, product):
         return product.find_element_by_class_name("price-standard")
@@ -38,6 +51,7 @@ class Scraper():
         return name_link.get_attribute("href")
 
     def find_element(self):
+        total_products = 0
         if self.root_element:
             products = self.root_element.find_elements_by_tag_name("li")
             for product in products:
@@ -45,9 +59,13 @@ class Scraper():
                 name_link = product.find_element_by_class_name("name-link")
                 name = self.get_name(name_link)
                 link = self.get_link(name_link)
-                print(price.text)
-                print(name)
-                print(link)
+                print(name, price.text, link)
+                total_products += 1
+                self.cursor.execute('INSERT INTO products (name, price, link) VALUES ( %s, %s, %s)',
+                                    (name, price.text, link))
+                self.connection.commit()
+
+            return total_products
 
     def __del__(self):
         if self.driver:
@@ -58,3 +76,4 @@ scraper = Scraper()
 scraper.find_element()
 
 del scraper
+
